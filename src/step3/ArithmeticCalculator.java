62097f39ad4d845ed9732b92f714a08705af2d3e
hoon3948 <robot3949@gmail.com>
2026-01-21 15:25:11 +0900

    4회차 시작

--- src/step3/ArithmeticCalculator.java
+++ src/step3/ArithmeticCalculator.java
@@ -1,32 +1,5 @@
 package step3;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-
 public class ArithmeticCalculator {
-    public List<Double> results = new ArrayList<>();
 
-    public <T extends Number, U extends Number> double calculate(T num1, U num2, OperatorType operator){
-        double double1 = num1.doubleValue();
-        double double2 = num2.doubleValue();
-        double result = operator.apply(double1, double2);
-
-        results.add(result);
-        return result;
-    }
-
-    public List<Double> getResults(){
-        return results;
-    }
-
-    public void removeResults(){
-        results.remove(0);
-    }
-
-    public List<Double> getBigResult(double value){
-        return results.stream()
-                .filter(v -> v > value)
-                .collect(Collectors.toList());
-    }
 }
--- src/step3/OperatorType.java
+++ src/step3/OperatorType.java
@@ -1,48 +1,5 @@
 package step3;
 
 public enum OperatorType {
-    SUM('+'){
-        @Override
-        public double apply(double num1, double num2) {
-            return num1 + num2;
-        }
-    },
-    SUB('-'){
-        @Override
-        public double apply(double num1, double num2) {
-            return num1 - num2;
-        }
-    },
-    MUL('*'){
-        @Override
-        public double apply(double num1, double num2) {
-            return num1 * num2;
-        }
-    },
-    DIV('/'){
-        @Override
-        public double apply(double num1, double num2) {
-            if(num2 == 0){
-                throw new ArithmeticException("0으로 나눌수없습니다.");
-            }
-            return num1 / num2;
-        }
-    };
-
-    private final char symbol;
-
-    OperatorType(char symbol){
-        this.symbol = symbol;
-    }
-    public abstract double apply(double num1,double num2);
-
-    public static OperatorType fromChar(char operator){
-        for(OperatorType op : values()){
-            if(op.symbol == operator){
-                return op;
-            }
-        }
-        throw new IllegalArgumentException("부호가 잘못 입력되었습니다.");
-    }
 
 }
