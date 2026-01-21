ac115548a38233108f2192de8071b5ef91fca6f3
hoon3948 <robot3949@gmail.com>
2026-01-21 17:16:49 +0900

    5회차 마지막 초기화

--- src/step3/App.java
+++ src/step3/App.java
@@ -5,44 +5,7 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        ArithmeticCalculator calculator = new ArithmeticCalculator();
 
-        String exit = " ";
-
-        while(!exit.equals("exit")) {
-            System.out.print("첫번째 숫자를 입력하시오.: ");
-            String input1 = sc.next();
-            Number num1 = input1.contains(".") ? Double.parseDouble(input1) : Integer.parseInt(input1);
-            System.out.print("계산한 부호를 입력하시오.: ");
-            char operator = sc.next().charAt(0);
-            OperatorType operatorType = OperatorType.fromChar(operator);
-            System.out.print("두번째 숫자를 입력하시오.: ");
-            String input2 = sc.next();
-            Number num2 = input2.contains(".") ? Double.parseDouble(input2) : Integer.parseInt(input2);
-
-            double result = calculator.calculate(num1,num2,operatorType);
-            System.out.println("결과: " + result);
-            System.out.println("결과 목록: " + calculator.getData());
-
-            System.out.print("오래된 결과를 지울까요?(yes입력): ");
-            String remove = sc.next();
-            if(remove.equals("yes")){
-                calculator.removeData();
-                System.out.println("결과 목록: " + calculator.getData());
-            }
-
-            System.out.print("보다 큰값들을 조회할까요?(yes 입력)");
-            String search = sc.next();
-            if (search.equals("yes")){
-                System.out.print("비교할 값을 입력하세요");
-                double value = sc.nextDouble();
-                System.out.println("결과: " + calculator.searchData(value));
-            }
-
-            System.out.print("계산을 끝낼까요?(exit 입력)");
-            exit = sc.next();
-        }
         sc.close();
     }
 }
--- src/step3/ArithmeticCalculator.java
+++ src/step3/ArithmeticCalculator.java
@@ -6,29 +6,5 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class ArithmeticCalculator {
-    public List<Double> data = new ArrayList<>();
 
-    public <T extends Number, U extends Number> double calculate(T num1, U num2, OperatorType operatorType){
-        double dnum1 = num1.doubleValue();
-        double dnum2 = num2.doubleValue();
-        double result = operatorType.apply(dnum1,dnum2);
-
-
-        data.add(result);
-        return result;
-    }
-
-    public List<Double> getData(){
-        return data;
-    }
-
-    public void removeData(){
-        data.remove(0);
-    }
-
-    public List<Double> searchData(double value){
-        return data.stream()
-                .filter(v -> v > value)
-                .collect(Collectors.toList());
-    }
 }
